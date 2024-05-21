70af586890a90cb9e31c8c0df5d0dec33dfc5b52
Duncanvk4118 <97104118@st.deltion.nl>
2024-05-21 17:23:03 +0200

    update

---
+++ storage/framework/views/c3e974953bd7ac2004f6a915b5946c4c.php
@@ -0,0 +1,27 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do List || HTML CSS JavaScript</title>
+
+    <link rel="stylesheet" href="toDoListStyle.css"/>
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+</head>
+<body>
+    <div id="display">
+        <div id="inputField">
+            <textarea id="inputTextArea" placeholder="Enter your new to-do"></textarea>
+            <i class="uil uil-notes note-icon"></i>
+        </div>
+        <ul id="toDoList"></ul>
+        <div id="pendingTasks">
+            <span>You have <span id="pendingNum">no </span> tasks pending. </span>
+            <button id="clearButton">Clear All</button>
+        </div>
+    </div>
+    <script src="toDoScript.js"></script>
+</body>
+</html>
+</body>
+</html>
+<?php /**PATH C:\Laravel\ToDoListProjectLaravelVue\resources\views\main.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/d777c9afdbd31c4d8fc0fa1e26b02f1c.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Login || HTML CSS JavaScript</title>
+
+    <link rel="stylesheet" href="<?php echo e(asset('assets/css/Loginstyle.css')); ?>">
+</head>
+
+<body>
+    <section id="display">
+        <div id="formSignUp">
+            <header id="SignUpHeader">Signup</header>
+            <form method="POST" action="">
+
+                <?php echo csrf_field(); ?>
+                <input type="text" name="name" placeholder="Full Name" required />
+                <input type="email" name="email" placeholder="Email" required />
+                <input type="password" name="password" placeholder="Password" required />
+                <input type="password" name="password_confirmation" placeholder="Confirm Password" required />
+                <div id="checkbox">
+                    <input type="checkbox" id="signUpCheckbox" required />
+                    <label for="signUpCheckbox">I accept all terms & conditions</label>
+                </div>
+                <input type="submit" value="Signup" />
+            </form>
+        </div>
+
+        <div id="formLogin">
+            <header id="LoginHeader">Login</header>
+            <form method="POST" action="">
+                <?php echo csrf_field(); ?>
+                <input type="email" name="email" placeholder="Email" required />
+                <input type="password" name="password" placeholder="Password" required />
+                <a href="">Forgot password?</a>
+                <input type="submit" value="Login" />
+            </form>
+        </div>
+
+        <script>
+            const display = document.getElementById("display");
+            const signUpHeader = document.getElementById("SignUpHeader");
+            const loginHeader = document.getElementById("LoginHeader");
+
+            function loginOnClickHeader() {
+                display.classList.add('active');
+            }
+            function signUpOnClickHeader() {
+                display.classList.remove('active');
+            }
+
+            loginHeader.addEventListener('click', loginOnClickHeader);
+            signUpHeader.addEventListener('click', signUpOnClickHeader);
+        </script>
+    </section>
+</body>
+
+</html><?php /**PATH C:\Laravel\ToDoListProjectLaravelVue\resources\views/login.blade.php ENDPATH**/ ?>
