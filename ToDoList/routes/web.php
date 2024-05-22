b1beef9249e13d2c93d2295b603deb4abaa52f69
Duncanvk4118 <97104118@st.deltion.nl>
2024-05-22 08:41:10 +0200

    added todo link

--- ToDoList/routes/web.php
+++ ToDoList/routes/web.php
@@ -11,6 +11,11 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+Route::get('/todo', function () {
+    return view('todo');
+})->middleware(['auth', 'verified'])->name('todo');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
