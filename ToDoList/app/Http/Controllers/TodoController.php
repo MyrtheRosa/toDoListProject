383a043766eccfc27e9fb7bac457a1426d415997
Duncanvk4118 <97104118@st.deltion.nl>
2024-05-23 09:15:44 +0200

    Model  / Cotroller

---
+++ ToDoList/app/Http/Controllers/TodoController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TodoController extends Controller
+{
+    public function create() {
+        //
+    }
+
+    public function delete() {
+        //
+    }
+
+    public function checked() {
+        //
+    }
+}
