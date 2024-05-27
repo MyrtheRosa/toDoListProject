e17062a64651b8a576a4e70a5d8dcbf6d64dca51
Duncanvk4118 <97104118@st.deltion.nl>
2024-05-27 11:42:37 +0200

    NO COM

--- ToDoList/resources/views/dashboard.blade.php
+++ ToDoList/resources/views/dashboard.blade.php
@@ -1,17 +1,24 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/selected.css') }}">
+    <title>Keuze Menu</title>
+</head>
+<body>
+<div class="selectors">
+    <div class="moon"></div>
+    <div class="container1">
+        <img onclick="redirectTime()" id="timed" src="{{asset('assets/images/todotimer.jpeg')}}" alt="">
+        <h1>ToDo list with timer</h1>
     </div>
-</x-app-layout>
+    <div class="container2">
+        <img onclick="redirectNoTime()" id="notimed" src="{{asset('assets/images/todonotime.jpg')}}" alt="">
+        <h1>ToDo list without timer</h1>
+    </div>
+</div>
+<script src="{{asset('assets/js/keuze.js')}}"></script>
+</body>
+</html>
