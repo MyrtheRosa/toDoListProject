d5af23fde9a125133f3e035734b174f05adb70af
Kevin Pierik <kevinpierik@icloud.com>
2024-05-27 10:43:22 +0200

    bramlover123

--- ToDoList/routes/web.php
+++ ToDoList/routes/web.php
@@ -8,27 +8,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::post('/todo', [TodoController::class, 'store'])->name('todo');
-Route::get('/todo', [TodoController::class, 'index'])->name('todo.get');
-
-
-Route::get('/keuze', function() {
-    return view('dashboard');
-})->name('keuze');
-
-Route::get('/todo/timer', function () {
-    return view('todotimer');
-})/*->middleware(['auth'])->name('todotimed')*/;
-
+Auth::routes();
 
 Route::middleware('auth')->group(function () {
+    Route::post('/todo', [TodoController::class, 'store'])->name('todo');
+    Route::get('/todo', [TodoController::class, 'index'])->name('todo.get');
+
+    Route::get('/keuze', function() {
+        return view('dashboard');
+    })->name('keuze');
+
+    Route::get('/todo/timer', function () {
+        return view('todotimer');
+    });
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-require __DIR__.'/auth.php';
-
-Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
