d0db149211cd38e2d9c7451566f00afb4c026df5
Duncanvk4118 <97104118@st.deltion.nl>
2024-05-27 11:13:07 +0200

    being cute

--- ToDoList/routes/web.php
+++ ToDoList/routes/web.php
@@ -1,30 +1,18 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TodoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Auth::routes();
-
-Route::middleware('auth')->group(function () {
-    Route::post('/todo', [TodoController::class, 'store'])->name('todo');
-    Route::get('/todo', [TodoController::class, 'index'])->name('todo.get');
-
-    Route::get('/keuze', function() {
-        return view('dashboard');
-    })->name('keuze');
-
-    Route::get('/todo/timer', function () {
-        return view('todotimer');
-    });
-
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
