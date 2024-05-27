6decde01a02063a0dc2129c2b22ed26218dc158b
Duncanvk4118 <97104118@st.deltion.nl>
2024-05-27 15:21:57 +0200

    Redirection to Kevin's Sims House

--- ToDoList/resources/views/auth/login.blade.php
+++ ToDoList/resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/auth/login.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;500;700&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -12,6 +12,11 @@
     <h2>Login</h2>
 </header>
 <div class="wrapper">
+    <section>
+        <div class="stars"></div>
+        <div class="moon"></div>
+        <div class="mountain_front"></div>
+    </section>
     <div class="form-box login">
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
@@ -36,23 +41,25 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
 
             <!-- Remember Me -->
-            <div class="remember-forgot">
-{{--                <label for="remember_me">--}}
-{{--                    <input id="remember_me" type="checkbox" name="remember">--}}
-{{--                    <span>{{ __('Remember me') }}</span>--}}
-{{--                </label>--}}
-                @if (Route::has('password.request'))
-                    <a href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
-                @endif
-            </div>
+            <section id="bottom">
+                <div class="remember-forgot">
+    {{--                <label for="remember_me">--}}
+    {{--                    <input id="remember_me" type="checkbox" name="remember">--}}
+    {{--                    <span>{{ __('Remember me') }}</span>--}}
+    {{--                </label>--}}
+                    @if (Route::has('password.request'))
+                        <a href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
+                    @endif
+                </div>
 
-            <button type="submit" class="btn">
-                {{ __('Log in') }}
-            </button>
+                <button type="submit" class="btn">
+                    {{ __('Log in') }}
+                </button>
 
-            <div class="login-register">
-                <p>{{ __("Don't have an account?") }} <a href="{{ route('register') }}">{{ __('Register') }}</a></p>
-            </div>
+                <div class="login-register">
+                    <p>{{ __("Don't have an account?") }} <a href="{{ route('register') }}">{{ __('Register') }}</a></p>
+                </div>
+            </section>
         </form>
     </div>
 </div>
