992f41f65dae8668bbcbf8ffaadd573487b96b98
Duncanvk4118 <97104118@st.deltion.nl>
2024-05-27 13:12:23 +0200

    Redirection to Kevin's Sims House

--- ToDoList/resources/views/todo.blade.php
+++ ToDoList/resources/views/todo.blade.php
@@ -25,8 +25,9 @@
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                 @csrf
             </form>
-            <button onclick="document.getElementById('logout-form').submit();">
+            <button id="logButton" onclick="document.getElementById('logout-form').submit();">
                 <i class="bi bi-box-arrow-right"></i>
+            </button>
             <h5>Settings</h5>
         </div>
 
