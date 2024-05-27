d0a49b05c8ca9b5590f4213a7016ab0d141bb7cf
Duncanvk4118 <97104118@st.deltion.nl>
2024-05-27 12:36:37 +0200

    NO COM

--- ToDoList/routes/web.php
+++ ToDoList/routes/web.php
@@ -22,6 +22,17 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/todo', function () {
+    return view('todo');
+})->middleware(['auth', 'verified'])->name('todo');
+
+Route::get('/todo/timer', function () {
+    return view('todotimer');
+})->middleware(['auth', 'verified'])->name('todotimer');
+
+
+
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
