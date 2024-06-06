b9bafd1800bd378e20c4f1676728bb0c485c69f4
Duncanvk4118 <97104118@st.deltion.nl>
2024-06-06 09:49:09 +0200

    edit completed

--- ToDoList/resources/views/profile/edit.blade.php
+++ ToDoList/resources/views/profile/edit.blade.php
@@ -1,32 +1,33 @@
+<header>
+    <link rel="stylesheet" href="{{ asset('assets/css/auth/edit.css')}}">
+</header>
+
 <x-app-layout>
-    <x-slot name="header" style="justify-content: space-between">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    <div name="header" class="Nav">
+        <h2 class="font-semibold text-xl leading-tight headerText">
             {{ __('Profile Information') }}
         </h2>
-        {{-- <button onclick="window.location = {{ URL::route('dashboard') }}">
-            Return to My ToDo List
-        </button> --}}
 
-        <x-responsive-nav-link :href="route('dashboard')">
+        <a href="./dashboard" id="redirect">
             {{ __('Return to My ToDo List') }}
-        </x-responsive-nav-link>
-    </x-slot>
+        </a>
+    </div>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+    <div class="py-12 normalBG">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6 items">
+            <div class="p-4 sm:p-8 sm:rounded-lg holder">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 sm:rounded-lg holder">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 sm:rounded-lg holder">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
