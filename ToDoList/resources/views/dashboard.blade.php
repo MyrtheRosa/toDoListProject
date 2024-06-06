6384160a94906332e80dfcb42ba338339895b88b
MyrtheRosa <myrthe-willemsen@hotmail.com>
2024-06-06 10:59:19 +0200

    slash spul weggehaald css

--- ToDoList/resources/views/dashboard.blade.php
+++ ToDoList/resources/views/dashboard.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,18 +8,20 @@
     <link rel="stylesheet" href="{{ asset('assets/css/selected.css') }}">
     <title>Keuze Menu</title>
 </head>
+
 <body>
-<div class="selectors">
-    <div class="moon"></div>
-    <div class="container1">
-        <img onclick="redirectTime()" id="timed" src="{{asset('assets/images/todotimer.jpeg')}}" alt="">
-        <h1>ToDo list with timer</h1>
+    <div class="selectors">
+        <div class="moon"></div>
+            <div class="container1">
+                <img onclick="redirectTime()" id="timed" src="{{asset('assets/images/todotimer.jpeg')}}" alt="">
+                <h1>ToDo list with timer</h1>
+            </div>
+            <div class="container2">
+                <img onclick="redirectNoTime()" id="notimed" src="{{asset('assets/images/todonotime.jpg')}}" alt="">
+                <h1>ToDo list without timer</h1>
+            </div>
     </div>
-    <div class="container2">
-        <img onclick="redirectNoTime()" id="notimed" src="{{asset('assets/images/todonotime.jpg')}}" alt="">
-        <h1>ToDo list without timer</h1>
-    </div>
-</div>
-<script src="{{asset('assets/js/keuze.js')}}"></script>
+    <script src="{{asset('assets/js/keuze.js')}}"></script>
 </body>
+
 </html>
